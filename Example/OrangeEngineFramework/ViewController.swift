6abbd789b8c0c0a7f0e04308f3169e09e892188e
一个橙子 <153190953@qq.com>
2020-06-27 17:11:05 +0800

    网络接口test

--- Example/OrangeEngineFramework/ViewController.swift
+++ Example/OrangeEngineFramework/ViewController.swift
@@ -14,14 +14,34 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let urlString = "http://58.246.9.10:19225/rest/sgmw/advertisement"
+        
         // Do any additional setup after loading the view, typically from a nib.
+
+//        OrangeEngineNetManager.shareManager.request(type: .Get, urlString: <#T##String#>, parameters: <#T##AnyObject?#>, successBlock: <#T##([String : Any]?) -> ()#>, failureBlock: <#T##(Error?) -> ()#>)(requestType: .GET, urlString: "http://58.246.9.10:19225/rest/sgmw/advertisement", parameters: ["userName": "zhangsan" as AnyObject]) { (json) in
+//            print(json as Any)
+//        }
         
-        print(OrangeEngineNetManager.ORANGE_SDK_VERSION)
         
-        
-        OrangeEngineNetManager.shared.request(requestType: .GET, urlString: "http://58.246.9.10:19225/rest/sgmw/advertisement", parameters: ["userName": "zhangsan" as AnyObject]) { (json) in
-            print(json as Any)
+        let param:[String : String] = [:]
+        OrangeEngineNetManager.shareManager.request(type: .Get, urlString: urlString, parameters: param as AnyObject, successBlock: { (dataDic) in
+//            print(dataDic ?? Dictionary())
+            
+//            print(dataDic!["entities"] as Any)
+                        
+            
+            if let listArray = dataDic?["entities"] as? NSArray {
+                for value in listArray {
+                    print(value)
+                }
+            }
+            
+        }) { (error) in
+            print(error ?? "")
+            
         }
+        
     }
 
     override func didReceiveMemoryWarning() {
--- OrangeEngineFramework/Classes/net/OrangeEngineNetManager.swift
+++ OrangeEngineFramework/Classes/net/OrangeEngineNetManager.swift
@@ -9,51 +9,47 @@ import Foundation
 import AFNetworking
 
 
-//枚举定义请求方式
-public enum HTTPRequestType {
-    case GET
-    case POST
+// 添加两种类型 Post，Get
+public enum requestType {
+    case Post
+    case Get
 }
+
 
 public class OrangeEngineNetManager:AFHTTPSessionManager {
     
-    public static let ORANGE_SDK_VERSION = "1.0.0"
-
     
     //单例
-    public static let shared = OrangeEngineNetManager()
-    
-    /// 封装GET和POST 请求
-    ///
-    /// - Parameters:
-    ///   - requestType: 请求方式
-    ///   - urlString: urlString
-    ///   - parameters: 字典参数
-    ///   - completion: 回调
-    ///调用的地方NetworkManager.shared.request(requestType: .GET, urlString: "https:www.baidu.com", parameters: ["userName": "zhangsan" as AnyObject]) { (json) in
-    /// print(json)}
-
+    public static let shareManager : OrangeEngineNetManager = {
+        let manager  = OrangeEngineNetManager()
+        manager.responseSerializer.acceptableContentTypes?.insert("text/html")
+        manager.responseSerializer.acceptableContentTypes?.insert("text/plain")
+        return manager
+    }()
     
     
-    public func request(requestType: HTTPRequestType, urlString: String, parameters: [String: AnyObject]?, completion: @escaping (AnyObject?) -> ()) {
-
-        //成功回调
-        let success = { (task: URLSessionDataTask, json: Any)->() in
-            completion(json as AnyObject?)
+    
+    // GET/POST
+    public func request(type: requestType, urlString: String, parameters: AnyObject?,successBlock: @escaping([String : Any]?) -> (), failureBlock: @escaping (Error?)->()) -> Void {
+        
+        
+        // 成功的闭包
+        let successBlock = { (task: URLSessionDataTask, responseObj: Any?) in
+            successBlock(responseObj as? [String : Any])
         }
-
-        //失败回调
-        let failure = { (task: URLSessionDataTask?, error: Error) -> () in
-            print("网络请求错误 \(error)")
-            completion(nil)
+        
+        // 失败的闭包
+        let failureBlock = { (task: URLSessionDataTask?, error: Error) in
+            failureBlock(error)
         }
-
-        _ = ["test":"test"]
-
-        if requestType == .GET {
-            get(urlString, parameters: parameters, progress: nil, success: success, failure: failure)
+        
+        // 进行请求
+        if type==requestType.Get {
+            // GET 担心包含中文处理一下
+            _ = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+            get(urlString, parameters: parameters, progress: nil, success: successBlock, failure: failureBlock)
         } else {
-            post(urlString, parameters: parameters, progress: nil, success: success, failure: failure)
+            post(urlString, parameters: parameters, progress: nil, success: successBlock, failure: failureBlock)
         }
     }
     
